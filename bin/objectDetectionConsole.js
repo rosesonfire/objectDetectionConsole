1a153f2bf31b74bb38119f8e6554ace691821e9f
ishti_ik <ishti_iks@yahoo.com>
2017-11-21 21:10:16 +0600

    commit

--- bin/objectDetectionConsole.js
+++ bin/objectDetectionConsole.js
@@ -2,7 +2,7 @@
 
 var { exec } = require("child_process");
 
-exec(`webpack-dev-server --content-base ${__dirname}/../app/images --config ${__dirname}/webpack.config.js`, function(err, stdout, stderr) {
+exec(`webpack-dev-server --content-base ${__dirname}/../app --config ${__dirname}/webpack.config.js`, function(err, stdout, stderr) {
   if (err) {
     console.log(err);
     return;
--- bin/webpack.config.js
+++ bin/webpack.config.js
@@ -1,46 +1,15 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var HtmlWebpackExcludeAssetsPlugin = require("html-webpack-exclude-assets-plugin");
-
-const template = "./../app/template.html";
 const scripts = "./../app/script.js";
-const styles = "./../app/style.css";
 const outputPath = __dirname + "/public";
 
 module.exports = {
   context: __dirname,
   entry: {
-    scripts: scripts,
-    styles: styles
+    scripts: scripts
   },
   output: {
     path: outputPath,
-    filename: "[name].min.js"
+    filename: "[name].pack.js"
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract([
-          {
-            loader: "css-loader",
-            options: {
-              minimize: true
-            }
-          }
-        ])
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin("[name].min.css"),
-    new HtmlWebpackPlugin({
-      hash: true,
-      template: template,
-      excludeAssets: [/styles.*js/]
-    }),
-    new HtmlWebpackExcludeAssetsPlugin()
-  ],
   devServer: {
     inline: true,
     open: true
